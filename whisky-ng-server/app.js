d101ebe4e84e910a4a22dad41e724694028147f0
jdandjinou <34840003+jdandjinou@users.noreply.github.com>
2020-05-29 13:37:59 -0400

    Connect to MongoDB with mongoose

--- whisky-ng-server/app.js
+++ whisky-ng-server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const api = require('./api/v1/index');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
 const port3000 = 3000;
 
 app.set('port', (process.env.PORT || port3000));
@@ -13,6 +15,17 @@ app.use((req, res) => {
 	err.status = 500;
 	res.json({msg: '404 - Not found!', err: err});
 });
-app.listen(app.get('port'), () => {
-	console.log(`The server listen to port ${app.get('port')}`);
+
+mongoose.connect('mongodb://localhost:27017/whiskyngserver', {useNewUrlParser: true});
+connection.on('error', (err) => {
+	console.error(`Connection to MongoDB error ${err.message}`);
 });
+
+connection.once('open', () => {
+	console.log('Connect to MongoDB');
+    
+	app.listen(app.get('port'), () => {
+		console.log(`The server listen to port ${app.get('port')}`);
+	});
+});
+
