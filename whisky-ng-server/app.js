0b874a08d1d87ed80baaceb97e5a219f1c21daf1
jdandjinou <34840003+jdandjinou@users.noreply.github.com>
2020-06-06 14:48:20 -0400

    Routes for pull request

--- whisky-ng-server/app.js
+++ whisky-ng-server/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/api/v1', api);
 app.use((req, res) => {
 	const err = new Error('404 - Not found!');
-	err.status = 500;
+	err.status = 400;
 	res.json({msg: '404 - Not found!', err: err});
 });
 
