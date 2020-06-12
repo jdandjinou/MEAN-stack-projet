3f4caf59c8d9a236a3946de57817ab4166e13acd
jdandjinou <34840003+jdandjinou@users.noreply.github.com>
2020-06-12 16:52:32 -0400

    Complete form creation

--- whisky-ng-client/src/app/blogpost-create/blogpost-create.component.ts
+++ whisky-ng-client/src/app/blogpost-create/blogpost-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-blogpost-create',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogpost-create.component.css']
 })
 export class BlogpostCreateComponent implements OnInit {
+  public creationForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.creationForm = this.fb.group({
+      title: '',
+      subTitle: '',
+      content: ''
+    })
+  }
+
+  public createBlogPost() {
+    if(this.creationForm.valid) {
+      console.log(this.creationForm.value);
+    }
+      
   }
 
 }
