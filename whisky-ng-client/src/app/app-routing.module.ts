0d97e0d1905b0abe34840e67a28677c8c96a1c76
jdandjinou <34840003+jdandjinou@users.noreply.github.com>
2020-06-21 11:39:28 -0400

    Edit a blog post

--- whisky-ng-client/src/app/app-routing.module.ts
+++ whisky-ng-client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 import { BlogpostComponent } from './blogpost/blogpost.component';
 import { ErrorPageComponent } from './errorpage/errorpage.component';
 import { AdminComponent } from './admin/admin.component';
+import { BlogpostEditComponent } from './blogpost-edit/blogpost-edit.component';
 
 
 const routes: Routes = [
   { path: '', component: BlogpostListComponent },
   { path: 'blog-posts/:id', component: BlogpostComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'admin/blog-posts/:id', component: BlogpostEditComponent },
   { path: '**', component: ErrorPageComponent}
 ];
 
--- whisky-ng-client/src/app/blogpost-edit/blogpost-edit.component.ts
+++ whisky-ng-client/src/app/blogpost-edit/blogpost-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogpostService } from '../blogpost.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormGroupDirective } from '@angular/forms';
 import { Blogpost } from '../models/blogpost';
 
 @Component({
@@ -20,7 +20,7 @@ export class BlogpostEditComponent implements OnInit {
               private el: ElementRef) { }
 
   ngOnInit(): void {
-    this.blogpostId = this.activateRoute.snapshot.params.get('id');
+    this.blogpostId = this.activateRoute.snapshot.paramMap.get('id');
     this.blogpostService
       .getBlogpostById(this.blogpostId)
       .subscribe(data => this.blogpost = data, error => console.error(error)
@@ -30,7 +30,7 @@ export class BlogpostEditComponent implements OnInit {
   }
 
   private createForm(): void {
-    this.fb.group({
+    this.editForm = this.fb.group({
       title: '',
       subTitle: '',
       content: '',
@@ -38,16 +38,20 @@ export class BlogpostEditComponent implements OnInit {
     });
   }
 
-  private upload(): void {
-    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#image');
+  public upload(): void {
+    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#blogImage');
     let fileCount: number = inputEl.files.length;
     let formData = new FormData();
     if(fileCount > 0) {
-      formData.append('image', inputEl.files.item(0));
+      formData.append('blogImage', inputEl.files.item(0));
       this.blogpostService
         .uploadImage(formData)
         .subscribe(data => console.log(data), error => console.error(error));
     }
+  }
+
+  public updateBlogpost(formDirective: FormGroupDirective): void {
+
   }
 
 }
